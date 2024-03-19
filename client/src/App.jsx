c1a2f2c5ce15a8ddf6056728064b4d61da465916
Yash09042004 <yashkiran2004@gmail.com>
2024-03-19 19:33:23 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Signup from './signup'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Login from './Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
+       <BrowserRouter>
+        <Routes>
+          <Route path='/register' element= {<Signup />}></Route>
+          <Route path='/login' element= {<Login />}></Route>
 
+        </Routes>
+       </BrowserRouter>
     </div>
       
   )
 }
+
 
 export default App
--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,18 +1,21 @@
+import React from 'react';
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from 'axios';
 
-function Signup() {
-    const [name,setName] = useState()
+function Login() {
     const [email,setEmail] = useState()
     const [password,setPassword] = useState()
     const navigate = useNavigate()
 
     const handleSubmit = (e) =>{
         e.preventDefault()
-        axios.post('http://localhost:3001/register', {name,email,password})
-        .then(result=> {console.log(result)
-        navigate('/login')
+        axios.post('http://localhost:3001/login', {email,password})
+        .then(result=> {
+            console.log(result)
+            if(result.data==="Success"){
+                navigate('/home')
+            }
         })
         .catch(err => console.error(err))
     }
@@ -20,21 +23,8 @@ function Signup() {
     return(
         <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
         <div className="bg-white p-3 rounded w-25">
-            <h2>Register</h2>
+            <h2>Login</h2>
             <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="email">
-                        <strong>Name</strong>
-                    </label>
-                    <input
-                    type = "text"
-                    placeholder="Enter Name"
-                    autoComplete="off"
-                    name = "email"
-                    className="form-control rounded-0"
-                    onChange={(e) => setName(e.target.value)}
-                    />
-                </div>
                 <div className="mb-3">
                     <label htmlFor="email">
                         <strong>Email</strong>
@@ -47,34 +37,32 @@ function Signup() {
                     className="form-control rounded-0"
                     onChange={(e) => setEmail(e.target.value)}
                     />
-                    </div>
+                </div>
                 
                 <div className="mb-3">
-                    <label htmlFor="email">
+                    <label htmlFor="password">
                         <strong>Password</strong>
                     </label>
                     <input
-                    type = "text"
+                    type = "password"
                     placeholder="Enter Password"
                     autoComplete="off"
-                    name = "email"
+                    name = "password"
                     className="form-control rounded-0"
                     onChange={(e) => setPassword(e.target.value)}
                     />
-                    </div>
-                    <button type="submit" className="btn btn-success w-100 rounded-0">
-                        Register
-                    </button>
-                    </form>
-                    <p>Already have an account</p>
-                    <Link to="/login" className="btn btn-default w-100 bg-light rounded-0 text-decoration-none">
-                        Login
-                    </Link>
-           
+                </div>
+                <button type="submit" className="btn btn-success w-100 rounded-0">
+                    Login
+                </button>
+            </form>
+            <p>Don't have an account?</p>
+            <Link to="/signup" className="btn btn-default w-100 bg-light rounded-0 text-decoration-none">
+                Signup
+            </Link>
         </div>
-
         </div>
     );
 }
 
-export default Signup;
+export default Login;
